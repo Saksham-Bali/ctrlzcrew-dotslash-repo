9b9b097bea4be650d7f57602e670302dd2034bf8
Manishbisht-bit <148936438+Manishbisht-bit@users.noreply.github.com>
2024-11-23 15:18:52 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -11,6 +11,16 @@ diabetes_dataset = pd.read_csv('diabetes.csv')
 diabetes_dataset.head() #checking what data contains
 
 # checking the number of rows and columns and all staistical measure of the data
-print("No of rows and columns:",diabetes_dataset.shape)
-print("Printing the statistical measures of the dataset: ")
-print(diabetes_dataset.describe())
+diabetes_dataset.shape
+diabetes_dataset.describe()
+
+# Separaitng the data and labels
+x = diabetes_dataset.drop(columns = 'Outcome',axis=1)
+y = diabetes_dataset['Outcome']
+
+# standardizing the data
+scaler = StandardScaler()
+scaler.fit(x)
+standardized_data = scaler.transform(x)
+x = standardized_data
+y = diabetes_dataset['Outcome']
