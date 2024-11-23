5c81112d0961087724273c2e71b489ef59d9a4af
Saksham Bali <sakshambali05@gmail.com>
2024-11-23 17:56:07 +0530

    parkinson edit

---
+++ parkison.py
@@ -0,0 +1,14 @@
+#importing libraries
+#to make arrays from dataset and work with them
+import numpy as np
+#to create dataframes from arrays and analyse it
+import pandas as pd
+#to split data into training and testing data
+from sklearn.model_selection import train_test_split
+#for standardizing the data
+from sklearn.preprocessing import StandardScaler
+#to implement the SVM(Support Vector Machine) model
+from sklearn import svm
+#to see the accuracy of the models
+from sklearn.metrics import accuracy_score
+
