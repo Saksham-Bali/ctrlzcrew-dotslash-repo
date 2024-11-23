3d31ff579ea4bdf82e99872fe20c455c1e04b40f
adityamasutey <149002768+adityamasutey@users.noreply.github.com>
2024-11-23 15:42:12 +0530

    Update heart diesease.py

--- heart diesease.py
+++ heart diesease.py
@@ -3,3 +3,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+#checking 'target' distribution
+heart_data['target'].value_counts()
+
+#seperating 'target' and other features
+x = heart_data.drop(columns='target',axis = 1)
+y = heart_data['target']
