1498d19546e74bbee1e89394edd6738044555270
adityamasutey <149002768+adityamasutey@users.noreply.github.com>
2024-11-23 16:46:39 +0530

    Update heart diesease.py

--- heart diesease.py
+++ heart diesease.py
@@ -20,3 +20,26 @@ model = LogisticRegression()
 
 #training logistic model with the training data
 model.fit(x_train,y_train)
+
+#training data accuracy
+x_train_prediction = model.predict(x_train)
+training_data_accuracy = accuracy_score(x_train_prediction,y_train)
+
+
+#test data accuracy
+x_test_prediction = model.predict(x_test)
+testing_data_accuracy = accuracy_score(x_test_prediction,y_test)
+
+#data prediction
+input_data = (37,1,2,130,250,0,1,187,0,3.5,0,0,2)
+# input_data = (37,0,0,128,205,0,1,130,0,3.5,0,0,2)
+
+np_array_form = np.asarray(input_data)
+reshaped_array = np_array_form.reshape(1,-1)
+prediction = model.predict(reshaped_array)
+# print(prediction)
+if(prediction[0] == 0):
+    print("Person does not have heart disease")
+else:
+    print("Person has heart disease")
+  
