a81b188c0e8a79a33bc9e1235841409c709da8be
adityamasutey <149002768+adityamasutey@users.noreply.github.com>
2024-11-23 16:16:21 +0530

    Update heart diesease.py

--- heart diesease.py
+++ heart diesease.py
@@ -10,3 +10,13 @@ heart_data['target'].value_counts()
 #seperating 'target' and other features
 x = heart_data.drop(columns='target',axis = 1)
 y = heart_data['target']
+
+
+#splitting data into training and testing data
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,stratify = y,random_state = 2)
+
+#model training
+model = LogisticRegression()
+
+#training logistic model with the training data
+model.fit(x_train,y_train)
