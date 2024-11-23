2ffb8ec38db8a77ee76b3e005676e2bd4a2aa169
Manishbisht-bit <148936438+Manishbisht-bit@users.noreply.github.com>
2024-11-23 16:57:01 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -31,3 +31,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.2, stratify 
 # Training the model
 classifier = svm.SVC(kernel='linear')
 classifier.fit(X_train,Y_train)
+
+# checking the working of the data 
+input_data = (4,110,92,0,0,37.6,0.191,30) #example data from the dataset
+data_into_array = np.asarray(input_data)
+reshaped_Data = data_into_array.reshape(1,-1)
+std_data = scaler.transform(reshaped_Data)
+output = classifier.predict(std_data)
+print(output)
